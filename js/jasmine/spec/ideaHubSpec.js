001f2c96919b4f9a1d2126a346c1c38b9b96c924
MakerSquare Student <students@themakersquare.com>
2013-09-26 08:04:42 -0500

    Add test to verify views are being loaded

--- js/jasmine/spec/ideaHubSpec.js
+++ js/jasmine/spec/ideaHubSpec.js
@@ -1,7 +1,31 @@
-describe("Let's load some templates!", function(){
-	it("it loads the external template!", function(){
-		loadFixtures("myfixture.html");
 
-		expect($("#my-fixture")).toExist();
+// Must diable redirects in script.js to run test.
+
+describe('updatePageInfo ', function(){
+	beforeEach(function(){
+		var tempObj = {
+			author: "author",
+			avatar: "avatar",
+			ideaTitle: "ideaTitle",
+			ideaDesc: "ideaDesc",
+			votes: "votes.length",
+			voted: "voted",
+			ideaId: "ideaId",
+			interest: "interest",
+			interested: "interested"
+		};
+		updatePageInfo(tempObj);
 	});
+
+	it("successfully loads a fake idea with div.author author === author", function(){
+		expect($(".author").text()).toBe("author");
+	});
+});
+
+
+
+describe('Test if views..', function () {
+  it("Are being populated on the page", function(){
+  	expect($('#ideas-feed')).toExist;
+  })
 });
