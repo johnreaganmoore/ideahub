71a04917a0186fb6de19ce92ab3f87bedc713d84
John Reagan Moore <johnreaganmoore@gmail.com>
2013-09-26 08:18:46 -0500

    Adds Users Interests to Profile Page

--- js/profile.js
+++ js/profile.js
@@ -54,7 +54,7 @@ var userIdeasView = new ShowUserIdeasView({
 
 var sample = {
 	data: {
-		title: "test title",
+		title: "Test Title",
 		votes: 4,
 		interest:7
 	}
@@ -62,7 +62,7 @@ var sample = {
 
 var sample1 = {
 	data: {
-		title: "Another title",
+		title: "Another Title",
 		votes: 6,
 		interest:3
 	}
@@ -71,3 +71,78 @@ var sample1 = {
 userIdeasView.add_new(sample);
 
 userIdeasView.add_new(sample1);
+
+
+//_________________User Interested Idea View created______________________
+
+var userInterestTemplateHtml = $('.templates .user-interests-template').html();
+var userInterestTemplate = _.template(userInterestTemplateHtml);
+
+
+var UserInterestView = Backbone.View.extend({
+
+	initialize: function(options) {
+		this.data = options.data;
+	},
+
+	render: function() {
+		var userInterestHtml = userInterestTemplate({
+			ideaTitle: this.data.title,
+			votes: this.data.votes,
+			response: this.data.response
+		});
+		$(this.el).html(userInterestHtml);
+		return this;
+	}
+
+});
+
+
+
+// Show IdeaViews created______________________
+
+var ShowUserInterestsView = Backbone.View.extend({
+
+	initialize: function(options) {
+		this.userInterestViews = [];
+	},
+
+	add_new: function(obj){
+		var newUserInterestView = new UserInterestView(obj);
+
+		var newUserInterestHtml = newUserInterestView.render().el;
+		this.userInterestViews.push(newUserInterestView);
+		$(this.el).append(newUserInterestHtml);
+	},
+
+	render: function(){
+		for(var i = 0; i < this.userInterestViews.length; i++){
+			var newUserInterestHtml = this.userInterestViews[i].render().el;
+			$(this.el).append(newUserInterestHtml);
+		}
+	}
+});
+
+var userInterestsView = new ShowUserInterestsView({
+	el: $('#user-interests')
+});
+
+var sample = {
+	data: {
+		title: "Test Title",
+		votes: 4,
+		response:"Denied"
+	}
+};
+
+var sample1 = {
+	data: {
+		title: "Another Title",
+		votes: 6,
+		response:"Accepted"
+	}
+};
+
+userInterestsView.add_new(sample);
+
+userInterestsView.add_new(sample1);
